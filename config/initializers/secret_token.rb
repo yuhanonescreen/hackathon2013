0a617615551132723990836e4ff3117981e8e9a6
Apex3 <apexrubydev@gmail.com>
2013-10-24 14:39:43 -0700

    initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hack::Application.config.secret_key_base = 'da6dcc982397a39cffa08f6d1393bd4412a3c7e2f13def3cf2f44f5b886858b2f297a9ca34f81d02a41afa35da9b168d6976ad3c6de0c88365aaa839b4521f31'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hack::Application.config.session_store :cookie_store, key: '_hack_session'
